df0d6b1d477ac3e329201d71eb6adc5870e881a8
mi-TSUKASA <mi.tsukasa0898@gmail.com>
2022-01-18 15:14:55 +0900

    configの設定をした configフォルダを作った main.goを作成した

--- config/config.go
+++ config/config.go
@@ -1,5 +1,33 @@
 package config
 
-func main() {
+import (
+	"log"
 
+	"gopkg.in/go-ini/ini.v1"
+)
+
+type ConfigList struct {
+	Port      string
+	SQLDriver string
+	DbName    string
+	LogFile   string
+}
+
+var Config ConfigList
+
+func init() {
+	LoadConfig()
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustStrig("8080"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+	}
 }
---
+++ go.mod
@@ -0,0 +1,14 @@
+module todo_app
+
+go 1.17
+
+replace config => ./config
+
+require config v0.0.0-00010101000000-000000000000
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/go-ini/ini.v1 v1.66.2 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
