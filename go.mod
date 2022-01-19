a4ebda6e18bace3bb1f5dd8f68d83b46e25f7d90
mi-TSUKASA <mi.tsukasa0898@gmail.com>
2022-01-19 19:39:19 +0900

    User.goを作成しました

--- go.mod
+++ go.mod
@@ -3,14 +3,20 @@ module todo_app
 go 1.17
 
 replace config => ./config
+
 replace utils => ./utils
+
+replace models => ./app/models
 
 require config v0.0.0-00010101000000-000000000000
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/go-ini/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	models v0.0.0-00010101000000-000000000000 // indirect
 	utils v0.0.0-00010101000000-000000000000 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"config"
+	"models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	/*
+		fmt.Println(config.Config.Port)
+		fmt.Println(config.Config.SQLDriver)
+		fmt.Println(config.Config.DbName)
+		fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+		log.Println("test")
+	*/
+
+	fmt.Println(models.Db)
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
+
+		u.CreateUser()
+	*/
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
+
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 
 }
