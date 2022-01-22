d3d67d97ec4af38e3881eab07d37200b1e939e16
aianman4823 <akitoaianman1@gmail.com>
2022-01-22 16:51:37 +0900

    generateHTML関数を作成

--- app/controllers/sever.go
+++ app/controllers/sever.go
@@ -2,8 +2,20 @@ package controllers
 
 import (
 	"config"
+	"fmt"
 	"net/http"
+	"text/template"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
