a449b25325bd2751c9ef83fcf08f8b5155f75716
mi-TSUKASA <mi.tsukasa0898@gmail.com>
2022-01-27 17:45:10 +0900

    topへのアクセス制限の設定

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -5,11 +5,17 @@ import (
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
+
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	sess, err := session(w, r)
+	_, err := session(w, r)
 	if err != nil {
 		http.Redirect(w, r, "/", 302)
 	} else {
